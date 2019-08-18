76e71f0ccfe6a5599061ab8874a55436ce298d41
Christoph Daube <christoph.daube@gmail.com>
2019-08-18 21:02:39 +0100

    added documentation

--- caxminmax.m
+++ caxminmax.m
@@ -1,4 +1,9 @@
 function caxminmax
+% changes the range of plotted values in an imagesc plot to abs(min max) of all values
+% combine this with fMap = flipud(cbrewer('div','RdBu',256)) as colourmap for nicest results!
+%
+% Christoph Daube, 2019, christoph.daube@gmail.com
+
     thsAxes = gca;
     thsLim = thsAxes.CLim;
     thsExt = max(abs(thsLim));
